e267792adfdea6be62320b0a5f8b433050e377d3
puronglong <1240429156@qq.com>
2016-02-16 15:21:48 +0800

    no message

--- angularjs/demo2/js/app.js
+++ angularjs/demo2/js/app.js
@@ -1,10 +1,10 @@
-var myApp = angular.module('myApp', ['ngRoute', 'ngAnimated']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate']);
 
 //现在，我们创建了我们的程序、路由以及控制器。
 //每一个控制器都有一个它自己的pageClass变量。改变了的值会被添加到index.html文件中的ng-view中，这样我们的每一个页面都有了不同的类名。
 //通过这些不同的类名，我们可以为不同的页面添加不同的动画效果。
 
-myApp.config(['$routerProvider', function($routeProvider){
+myApp.config(['$routeProvider', function($routeProvider){
 	$routeProvider
 			.when('/', {
 				templateUrl: 'Page-home.html',
