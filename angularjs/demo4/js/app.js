979b4bdc63774d0b7e68b91a70da498c4c9f1fab
puronglong <1240429156@qq.com>
2016-02-22 14:40:31 +0800

    no message

--- angularjs/demo4/js/app.js
+++ angularjs/demo4/js/app.js
@@ -3,6 +3,5 @@ var myApp = angular.module('myApp', ['ui.router', 'ngAnimate']);
 myApp.controller('viewController', viewController);
 
 function  viewController(){
-	var view = this;
-	view.current = '1';
+	this.current = '1';
 }
