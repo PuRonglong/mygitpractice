8f823d94a9a0fadef04d2db69789153155ff3f54
puronglong <1240429156@qq.com>
2016-02-22 13:38:37 +0800

    no message

--- angularjs/demo4/js/app.js
+++ angularjs/demo4/js/app.js
@@ -1,1 +1,8 @@
-var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate']);
+var myApp = angular.module('myApp', ['ui.router', 'ngAnimate']);
+
+myApp.controller('viewController', viewController);
+
+function  viewController(){
+	var view = this;
+	view.current = '1';
+}
