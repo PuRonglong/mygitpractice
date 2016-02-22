091af98b0487df68400363065193982da5f5fac1
puronglong <1240429156@qq.com>
2016-02-22 14:54:43 +0800

    no message

--- angularjs/demo4/js/app.js
+++ angularjs/demo4/js/app.js
@@ -1,7 +1,5 @@
 var myApp = angular.module('myApp', ['ui.router', 'ngAnimate']);
 
-myApp.controller('viewController', viewController);
-
-function  viewController(){
+myApp.controller('viewController', function(){
 	this.current = '1';
-}
+});
