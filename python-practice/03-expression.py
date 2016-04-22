78156ae7813cc7c5d813c479cead690c95645435
puronglong <1240429156@qq.com>
2016-04-22 20:48:24 +0800

    no message

---
+++ python-practice/03-expression.py
@@ -0,0 +1,5 @@
+length = 5
+width = 2
+area = length * width
+print 'Area is', area
+print 'Perimeter is', 2 * (length + width)
---
+++ python-practice/04-if.py
@@ -0,0 +1,7 @@
+number = 23
+guess = int(raw_input('Enter an integer : '))
+if guess == number:
+	print 'Congratulations, you guessed it.'
+else:
+	print 'No, it is a little lower than that'
+print 'Done'
