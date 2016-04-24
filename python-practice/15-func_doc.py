7de5d948e8bafdec54d23d7d1fe8bda3ff555de8
puronglong <1240429156@qq.com>
2016-04-24 19:03:07 +0800

    message

---
+++ python-practice/15-func_doc.py
@@ -0,0 +1,13 @@
+def printMax(x, y):
+	'''Prints the maximum of two numbers
+
+	the two numbers must be integers.'''
+
+	if x > y:
+		print x
+	else:
+		print y
+
+printMax(3, 4)
+
+print printMax.__doc__
