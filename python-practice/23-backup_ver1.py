53bcc6b1a221ddd0d8407dae180ec241ceae9b5b
puronglong <1240429156@qq.com>
2016-04-25 13:06:02 +0800

    backup_ver1

--- python-practice/23-backup_ver1.py
+++ python-practice/23-backup_ver1.py
@@ -5,10 +5,9 @@ source = ['/Users/puronglong/Desktop/demo']
 
 target_dir = '/Users/puronglong/Desktop/'
 
-target = target_dir + time.strftime('%Y%m%d%H%M%S') + '.zip'
+target = target_dir + time.strftime('%Y%m%d%H%M%S') + '-demo' + '.zip'
 
-zip_command = "zip -qr '%s' %s" % (target, ' '.join(source))
-
+zip_command = "zip -qr '%s' %s" %(target, ' '.join(source))
 
 if os.system(zip_command) == 0:
 	print 'Successful backup to', target
