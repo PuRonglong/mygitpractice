bfffed10cd98f2df12bac95bbfbe4d349bd7bbfb
puronglong <puronglong@foxmail.com>
2016-04-28 23:22:07 +0800

    qinshimoon baidu tieba spider

---
+++ python-practice/web spider/1-urllib2_test.py
@@ -0,0 +1,7 @@
+import urllib2
+
+response = urllib2.urlopen('http://www.baidu.com')
+
+html = response.read()
+
+print html
---
+++ python-practice/web spider/2-qinshimoon_tieba.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import string, urllib2
+
+def qinshi_tieba(url, page_begin, page_end):
+	for i in range(page_begin, page_end + 1):
+		sName = string.zfill(i, 5) + '.html'
+		print '正在下载第' + str(i) + '页，并存储为' + sName + '...'
+		f = open(sName, 'w+')
+		m = urllib2.urlopen(url + str(i)).read()
+		f.write(m)
+		f.close()
+	
+
+
+# url = 'http://tieba.baidu.com/p/4510793112?pn='
+
+url = str(raw_input('输入贴吧地址：'))
+
+page_begin = int(raw_input('请输入开始页：'))
+
+page_end = int(raw_input('结束页：'))
+
+qinshi_tieba(url, page_begin, page_end)
