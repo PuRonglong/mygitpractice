4c3328206b0c9642c15ad5aa7c304d7652e4fb3d
puronglong <1240429156@qq.com>
2016-08-07 19:02:31 +0800

    no message

--- materialize/js/index.js
+++ materialize/js/index.js
@@ -2,6 +2,23 @@
  * author: puronglong
  */
 
+$(document).ready(function () {
+	'use strict';
+	$('.slider').slider({
+		full_width: true,
+		indicators: true,  // 导航标识
+		interval: 7000,   // 间隔时间设置
+		transition: 700    // 持续时间设置
+	});
 
-$('.slider').slider({full_width: true});
+	$('.button-previous-left').on('click', function () {
+		$('.slider').slider('prev');
+	});
+
+	$('.button-previous-right').on('click', function () {
+		$('.slider').slider('next');
+	});
+});
+
+
 
